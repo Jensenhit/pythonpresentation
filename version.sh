83a3645b1e1a99f417a8bbe5b9b326a5be342ac9
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-18 20:50:16 +0800

    Add files via upload

---
+++ version.sh
@@ -0,0 +1,15 @@
+
+# This script is sourced to define the main version parameters
+
+# The main version
+KLAYOUT_VERSION="0.26"
+
+# The build date
+KLAYOUT_VERSION_DATE=$(date "+%Y-%m-%d")
+
+# The short SHA hash of the commit
+KLAYOUT_VERSION_REV=$(git rev-parse --short HEAD 2>/dev/null)
+
+if [ "$KLAYOUT_VERSION_REV" = "" ]; then
+	KLAYOUT_VERSION_REV="LatestSourcePackage"
+fi
